2365373a057a3157049b9381cb4f0bd2f4453443
Gourav Sardana <gsardana001@gmail.com>
2020-05-27 14:19:31 +0530

    dj-static config

--- ft_django_rest/wsgi.py
+++ ft_django_rest/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ft_django_rest.settings")
 
 application = get_wsgi_application()
+
+
+application = Cling(get_wsgi_application())
